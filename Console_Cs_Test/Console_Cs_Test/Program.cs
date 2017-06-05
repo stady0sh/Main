4623a88db956876a50a9769c9a67baa4d6f15f11
stady0sh <stad.y0sh@gmail.com>
2017-06-05 13:00:47 +0900

    -

--- Console_Cs_Test/Console_Cs_Test/Program.cs
+++ Console_Cs_Test/Console_Cs_Test/Program.cs
@@ -1,24 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 
-class Program
-{    
+public class Program
+{
+    public void Proc()
+    {
+        Reader.IsDebug = false;
+        string inpt = Reader.ReadLine();
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < inpt.Length; i++)
+        {
+            char c = (char)(inpt[i] - ((i + 1) % ('Z' - 'A' + 1)));
+            if (c < 'A')
+            {
+                c = (char)(c - 'A' + 'Z' + 1);
+            }
+            ans.Append(c);
+        }
+        Console.WriteLine(ans.ToString());
+    }
+
     public static void Main()
     {
-        char[] S = Console.ReadLine().ToArray();
-        char[] R = new Char[S.Length];
-        for (int i = 0;i < S.Length; ++i)
-        {
-            R[i] = ToAscii(S[i],i + 1);
-        }
-        Console.WriteLine($"{string.Join("",R)}");
-    }    
-
-    private static char ToAscii(char c,int n)
-    {
-        int code = (int)c + 26 - (n % 26);
-        if (code > 90) { code -= 26; }
-        return (char)(code);
+        Program prg = new Program();
+        prg.Proc();
     }
 }
+
+class Reader
+{
+    public static bool IsDebug = true;
+
+    private static System.IO.StringReader sr;
+
+    public static string ReadLine()
+    {
+        if (IsDebug)
+        {
+            if (sr == null)
+            {
+                sr = new System.IO.StringReader(initStr.Trim());
+            }
+            return sr.ReadLine();
+        }
+        else {
+            return Console.ReadLine();
+        }
+    }
+
+    public static int[] GetInt(char delimiter = ' ')
+    {
+        string[] inpt = ReadLine().Split(delimiter);
+        int[] ret = new int[inpt.Length];
+        for (int i = 0; i < inpt.Length; i++)
+        {
+            ret[i] = int.Parse(inpt[i]);
+        }
+        return ret;
+    }
+
+    private static string initStr = @"
+
+BCD
+
+";
+}
+
---
+++ YukiCoder_Cs/YukiCoder_Cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YukiCoder_Cs
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int p = 3;
+            int 誤差 = 1000000;
+            double n = double.Parse(Console.ReadLine());
+            double ans = Math.Sqrt(n / p);
+            Console.WriteLine(Math.Ceiling(ans / 誤差) * 誤差);
+        }
+    }
+}
