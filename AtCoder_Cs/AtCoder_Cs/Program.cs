a44500950cbe5f2c0baca455bd1d336e57edaa7e
Y-DeskTop <Y-DeskTop@Y-DESKTOP-PC>
2018-10-14 23:37:19 +0900

    1

--- AtCoder_Cs/AtCoder_Cs/Program.cs
+++ AtCoder_Cs/AtCoder_Cs/Program.cs
@@ -15,32 +15,14 @@ namespace AtCoder_Cs
 
         public static void Main()
         {
-            var m = ReadInt32();
+			var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+			Array.Sort(a);
 
-            var ret = 0;
-
-            if (m < 100)
-            {
-                ret = 0;
-            }
-            else if (m < 5000)
-            {
-                ret = m / 1000 * 10;
-            }
-            else if (m < 30000)
-            {
-
-            }
-            else if (m < 70000)
-            {
-
-            }
-            else
-            {
-
-            }
-
-            Console.WriteLine($"{ret,00}");
+			var ret = 0;
+			ret += Math.Abs(a[0] - a[1]);
+			ret += Math.Abs(a[1] - a[2]);
+			
+			Console.WriteLine($"{ret}");
 
         }
     }
